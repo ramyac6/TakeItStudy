6e12755a524a75def4ced24a062ac67df65a02f7
Ramya Challa <ramyac6@cs.washington.edu>
2023-06-14 13:17:07 -0700

    Add final study files

---
+++ Take_it_study.pml
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8" ?>
+<Package name="Take_it_study" format_version="4">
+    <Manifest src="manifest.xml" />
+    <BehaviorDescriptions>
+        <BehaviorDescription name="behavior" src="_Wide" xar="behavior.xar" />
+        <BehaviorDescription name="behavior" src="_High" xar="behavior.xar" />
+        <BehaviorDescription name="behavior" src="_Low" xar="behavior.xar" />
+        <BehaviorDescription name="behavior" src="__rest" xar="behavior.xar" />
+        <BehaviorDescription name="behavior" src="_Direct" xar="behavior.xar" />
+    </BehaviorDescriptions>
+    <Dialogs />
+    <Resources />
+    <Topics />
+    <IgnoredPaths />
+    <Translations auto-fill="en_US">
+        <Translation name="translation_en_US" src="translations/translation_en_US.ts" language="en_US" />
+    </Translations>
+</Package>
---
+++ translations/translation_en_US.ts
@@ -0,0 +1,88 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>Manual/behavior.xar:/Say</name>
+        <message>
+            <source>Program Started</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Program Started</translation>
+        </message>
+    </context>
+    <context>
+        <name>Manual/behavior.xar:/Say (1)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hello</translation>
+        </message>
+    </context>
+    <context>
+        <name>Menu/behavior.xar:/Animated Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Program starting</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Program starting</translation>
+        </message>
+    </context>
+    <context>
+        <name>Menu/behavior.xar:/Say</name>
+        <message>
+            <source>Program Started</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Program Started</translation>
+        </message>
+    </context>
+    <context>
+        <name>Menu/behavior.xar:/Say (1)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Starting program</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Starting program</translation>
+        </message>
+        <message>
+            <source>Program starting</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Program starting</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Program Started</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Program Started</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (1)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+    </context>
+</TS>
